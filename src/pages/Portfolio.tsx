860307ded2d257233e09db38372e26296d8a0781
airleibiz <airleibiz@gmail.com>
2025-11-25 17:33:07 +0800

    Update Portfolio.tsx

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -26,7 +26,7 @@ const Portfolio: React.FC = () => {
       id: 1,
       title: 'FIRE CHARITY',
       thumbnail: "/images/FIRE.png",
-      video: "https://youtu.be/uEMyvax1kAw"
+      video: "https://youtu.be/embed/uEMyvax1kAw"
     },
     {
       id: 2,
